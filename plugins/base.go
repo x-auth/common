c667ca643c4a1811ba4ca09f77d22f3a49c0ca3d
Nictec <nicnac9911@gmail.com>
2022-02-17 08:52:26 +0100

    Update base.go

--- plugins/base.go
+++ plugins/base.go
@@ -3,5 +3,5 @@ package plugins
 import "github.com/x-auth/common/models"
 
 type AuthPlugin interface{
-  Login(username string, password string) (models.Profile, bool)
+  Login(username string, password string) (models.Profile, error)
 }
